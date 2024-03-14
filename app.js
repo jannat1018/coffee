2582f435a4277f042b57b50548181c55c3c1dd2a
jannat1018 <jannatruhi114@gmail.com>
2024-03-14 11:14:01 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cardItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cardItem.classList.remove('active');
+}
+
+
+let cardItem = document.querySelector('.card-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cardItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cardItem.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
